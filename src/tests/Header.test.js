738ba10a5c65cb2155117ab53adb1457777c264e
Jasem Duncan <jasemvalencia@gmail.com>
2022-01-14 20:08:52 -0500

    make test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,14 +1,15 @@
-// import { render, screen, cleanup } from '@testing-library/react';
-// import { Provider } from 'react-redux';
-// import store from '../Redux/store';
-// import Header from '../components/Header';
-
-// afterEach(() => {
-//   cleanup();
-// });
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Header from '../components/Header';
 
 describe('render  component to the DOM', () => {
   test('render Header component', async () => {
-    // render(<Provider store={store}><Header /></Provider>);
+    const tree = renderer.create(
+      <MemoryRouter> 
+        <Header />
+      </MemoryRouter>,
+    ).toJSON;
+    expect(tree).toMatchSnapshot();        
   });
 });
